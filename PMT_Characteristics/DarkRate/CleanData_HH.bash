8376ed7a9df83d35bf7163dbe497071259af33d7
hhuckvale <hollymae.huckvale@gmail.com>
2026-01-30 16:38:05 +0000

    sorting out all time above threshold

--- PMT_Characteristics/DarkRate/CleanData_HH.bash
+++ PMT_Characteristics/DarkRate/CleanData_HH.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-input_folder="PMT1_Processed_16" 
-output_folder="PMT1_Cleaned_16"
+input_folder="CORRECTED_INDICES_processed" 
+output_folder="CORRECTED_INDICES_AMPCUTS"
 
 for csv_file in "$input_folder"/*.csv;
 do
     base_name=$(basename "$csv_file" .csv)
     echo "Cleaning $base_name.csv"
 
-    python FINALDataCleaning.py "$input_folder" "$base_name" "$output_folder"
+    python FINALDataCleaning_extracutsonamp.py "$input_folder" "$base_name" "$output_folder"
 
 done
